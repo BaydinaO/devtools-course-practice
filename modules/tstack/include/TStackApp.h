cb90814531a9c13b6c00097f5ece43c76b14c0ac
Baydina O <ol4ik-12@mail.ru>
2016-06-02 23:32:17 +0400

    second

--- modules/tstack/include/TStackApp.h
+++ modules/tstack/include/TStackApp.h
@@ -1,16 +1,16 @@
 // Copyright 2016 Baydina Olya
 
-#ifndef MODULES_TSTACKAPPLICATION_INCLUDE_TSTACKAPP_H_
-#define MODULES_TSTACKAPPLICATION_INCLUDE_TSTACKAPP_H_
+#ifndef MODULES_TSTACK_INCLUDE_TSTACKAPP_H_
+#define MODULES_TSTACK_INCLUDE_TSTACKAPP_H_
 
 #include <string>
 
 class TStackApp {
-public:
+ public:
     TStackApp();
     std::string operator()(int argc, const char** argv);
 
-private:
+ private:
     void help(const char* appname, const char* message = "");
     bool validateNumberOfArguments(int argc, const char** argv);
     std::string message_;
@@ -20,7 +20,7 @@ private:
     } Arguments;
 };
 
-#endif  // MODULES_TSTACKAPPLICATION_INCLUDE_TSTACKAPP_H_
+#endif  // MODULES_TSTACK_INCLUDE_TSTACKAPP_H_
 
 
 
